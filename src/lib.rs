8d0dda08479fb6baa38544d8a236715563575df0
Isis Lovecruft <isis@torproject.org>
2017-11-05 23:20:04 +0000

    Implement serde serialisation support and pre-expanded secret keys.
     * ADD support for serialisation with serde.
     * ADD a new pre-expanded secret key type, `ExpandedSecretKey`.
     * FIXES Issue #13:
       https://github.com/isislovecruft/ed25519-dalek/issues/13

--- src/lib.rs
+++ src/lib.rs
@@ -96,17 +96,165 @@
 //! # let keypair: Keypair = Keypair::generate::<Sha512>(&mut cspring);
 //! # let message: &[u8] = "This is a test of the tsunami alert system.".as_bytes();
 //! # let signature: Signature = keypair.sign::<Sha512>(message);
+//!
 //! let public_key: PublicKey = keypair.public;
 //! let verified: bool = public_key.verify::<Sha512>(message, &signature);
 //!
 //! assert!(verified);
 //! # }
 //! ```
+//!
+//! ## Serialisation
+//!
+//! `PublicKey`s, `SecretKey`s, `Keypair`s, and `Signature`s can be serialised
+//! into byte-arrays by calling `.to_bytes()`.  It's perfectly acceptible and
+//! safe to transfer and/or store those bytes.  (Of course, never transfer your
+//! secret key to anyone else, since they will only need the public key to
+//! verify your signatures!)
+//!
+//! ```
+//! # extern crate rand;
+//! # extern crate sha2;
+//! # extern crate ed25519_dalek;
+//! # fn main() {
+//! # use rand::{Rng, OsRng};
+//! # use sha2::Sha512;
+//! # use ed25519_dalek::{Keypair, Signature, PublicKey};
+//! use ed25519_dalek::{PUBLIC_KEY_LENGTH, SECRET_KEY_LENGTH, KEYPAIR_LENGTH, SIGNATURE_LENGTH};
+//! # let mut cspring: OsRng = OsRng::new().unwrap();
+//! # let keypair: Keypair = Keypair::generate::<Sha512>(&mut cspring);
+//! # let message: &[u8] = "This is a test of the tsunami alert system.".as_bytes();
+//! # let signature: Signature = keypair.sign::<Sha512>(message);
+//! # let public_key: PublicKey = keypair.public;
+//! # let verified: bool = public_key.verify::<Sha512>(message, &signature);
+//!
+//! let public_key_bytes: [u8; PUBLIC_KEY_LENGTH] = public_key.to_bytes();
+//! let secret_key_bytes: [u8; SECRET_KEY_LENGTH] = keypair.secret.to_bytes();
+//! let keypair_bytes:    [u8; KEYPAIR_LENGTH]    = keypair.to_bytes();
+//! let signature_bytes:  [u8; SIGNATURE_LENGTH]  = signature.to_bytes();
+//! # }
+//! ```
+//!
+//! And similarly, decoded from bytes with `::from_bytes()`:
+//!
+//! ```
+//! # extern crate rand;
+//! # extern crate sha2;
+//! # extern crate ed25519_dalek;
+//! # use rand::{Rng, OsRng};
+//! # use sha2::Sha512;
+//! # use ed25519_dalek::{Keypair, Signature, PublicKey, SecretKey};
+//! # use ed25519_dalek::{PUBLIC_KEY_LENGTH, SECRET_KEY_LENGTH, KEYPAIR_LENGTH, SIGNATURE_LENGTH};
+//! # fn do_test() -> Result<(SecretKey, PublicKey, Keypair, Signature), &'static str> {
+//! # let mut cspring: OsRng = OsRng::new().unwrap();
+//! # let keypair_orig: Keypair = Keypair::generate::<Sha512>(&mut cspring);
+//! # let message: &[u8] = "This is a test of the tsunami alert system.".as_bytes();
+//! # let signature_orig: Signature = keypair_orig.sign::<Sha512>(message);
+//! # let public_key_bytes: [u8; PUBLIC_KEY_LENGTH] = keypair_orig.public.to_bytes();
+//! # let secret_key_bytes: [u8; SECRET_KEY_LENGTH] = keypair_orig.secret.to_bytes();
+//! # let keypair_bytes:    [u8; KEYPAIR_LENGTH]    = keypair_orig.to_bytes();
+//! # let signature_bytes:  [u8; SIGNATURE_LENGTH]  = signature_orig.to_bytes();
+//! #
+//! let public_key: PublicKey = PublicKey::from_bytes(&public_key_bytes)?;
+//! let secret_key: SecretKey = SecretKey::from_bytes(&secret_key_bytes)?;
+//! let keypair:    Keypair   = Keypair::from_bytes(&keypair_bytes)?;
+//! let signature:  Signature = Signature::from_bytes(&signature_bytes)?;
+//! #
+//! # Ok((secret_key, public_key, keypair, signature))
+//! # }
+//! # fn main() {
+//! #     do_test();
+//! # }
+//! ```
+//!
+//! ### Using Serde
+//!
+//! If you prefer the bytes to be wrapped in another serialisation format, all
+//! types additionally come with built-in [serde](https://serde.rs) support by
+//! building `ed25519-dalek` via:
+//!
+//! ```ignore,bash
+//! $ cargo build --features="serde"
+//! ```
+//!
+//! They can be then serialised into any of the wire formats which serde supports.
+//! For example, using [bincode](https://github.com/TyOverby/bincode):
+//!
+//! ```
+//! # extern crate rand;
+//! # extern crate sha2;
+//! # extern crate ed25519_dalek;
+//! # #[cfg(feature = "serde")]
+//! extern crate serde;
+//! # #[cfg(feature = "serde")]
+//! extern crate bincode;
+//!
+//! # #[cfg(feature = "serde")]
+//! # fn main() {
+//! # use rand::{Rng, OsRng};
+//! # use sha2::Sha512;
+//! # use ed25519_dalek::{Keypair, Signature, PublicKey};
+//! use bincode::{serialize, Infinite};
+//! # let mut cspring: OsRng = OsRng::new().unwrap();
+//! # let keypair: Keypair = Keypair::generate::<Sha512>(&mut cspring);
+//! # let message: &[u8] = "This is a test of the tsunami alert system.".as_bytes();
+//! # let signature: Signature = keypair.sign::<Sha512>(message);
+//! # let public_key: PublicKey = keypair.public;
+//! # let verified: bool = public_key.verify::<Sha512>(message, &signature);
+//!
+//! let encoded_public_key: Vec<u8> = serialize(&public_key, Infinite).unwrap();
+//! let encoded_signature: Vec<u8> = serialize(&signature, Infinite).unwrap();
+//! # }
+//! # #[cfg(not(feature = "serde"))]
+//! # fn main() {}
+//! ```
+//!
+//! After sending the `encoded_public_key` and `encoded_signature`, the
+//! recipient may deserialise them and verify:
+//!
+//! ```
+//! # extern crate rand;
+//! # extern crate sha2;
+//! # extern crate ed25519_dalek;
+//! # #[cfg(feature = "serde")]
+//! # extern crate serde;
+//! # #[cfg(feature = "serde")]
+//! # extern crate bincode;
+//! #
+//! # #[cfg(feature = "serde")]
+//! # fn main() {
+//! # use rand::{Rng, OsRng};
+//! # use sha2::Sha512;
+//! # use ed25519_dalek::{Keypair, Signature, PublicKey};
+//! # use bincode::{serialize, Infinite};
+//! use bincode::{deserialize};
+//!
+//! # let mut cspring: OsRng = OsRng::new().unwrap();
+//! # let keypair: Keypair = Keypair::generate::<Sha512>(&mut cspring);
+//! let message: &[u8] = "This is a test of the tsunami alert system.".as_bytes();
+//! # let signature: Signature = keypair.sign::<Sha512>(message);
+//! # let public_key: PublicKey = keypair.public;
+//! # let verified: bool = public_key.verify::<Sha512>(message, &signature);
+//! # let encoded_public_key: Vec<u8> = serialize(&public_key, Infinite).unwrap();
+//! # let encoded_signature: Vec<u8> = serialize(&signature, Infinite).unwrap();
+//! let decoded_public_key: PublicKey = deserialize(&encoded_public_key).unwrap();
+//! let decoded_signature: Signature = deserialize(&encoded_signature).unwrap();
+//!
+//! # assert_eq!(public_key, decoded_public_key);
+//! # assert_eq!(signature, decoded_signature);
+//! #
+//! let verified: bool = decoded_public_key.verify::<Sha512>(&message, &decoded_signature);
+//!
+//! assert!(verified);
+//! # }
+//! # #[cfg(not(feature = "serde"))]
+//! # fn main() {}
+//! ```
 
 #![no_std]
 #![cfg_attr(feature = "nightly", feature(rand))]
-#![allow(unused_features)]
 #![cfg_attr(feature = "bench", feature(test))]
+#![allow(unused_features)]
 #![deny(missing_docs)] // refuse to compile if documentation is missing
 
 #[macro_use]
@@ -123,7 +271,7 @@ extern crate rand;
 #[macro_use]
 extern crate std;
 
-#[cfg(test)]
+#[cfg(any(test, feature = "sha2"))]
 extern crate sha2;
 
 #[cfg(test)]
@@ -132,6 +280,11 @@ extern crate hex;
 #[cfg(all(test, feature = "bench"))]
 extern crate test;
 
+#[cfg(feature = "serde")]
+extern crate serde;
+
+#[cfg(all(test, feature = "serde"))]
+extern crate bincode;
 
 mod ed25519;
 

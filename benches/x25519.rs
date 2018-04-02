659d6c258e756b52428c8d542dacca9431f1d4f2
Isis Lovecruft <isis@torproject.org>
2018-04-02 21:53:26 +0000

    Add criterion benchmark for diffie_hellman() function.

---
+++ benches/x25519.rs
@@ -0,0 +1,46 @@
+// -*- mode: rust; -*-
+//
+// This file is part of x25519-dalek.
+// Copyright (c) 2017 Isis Lovecruft
+// See LICENSE for licensing information.
+//
+// Authors:
+// - Isis Agora Lovecruft <isis@patternsinthevoid.net>
+
+//! Benchmark the Diffie-Hellman operation.
+
+#[macro_use]
+extern crate criterion;
+extern crate rand;
+extern crate x25519_dalek;
+
+use criterion::Criterion;
+
+use rand::OsRng;
+
+use x25519_dalek::generate_public;
+use x25519_dalek::generate_secret;
+use x25519_dalek::diffie_hellman;
+
+fn bench_diffie_hellman(c: &mut Criterion) {
+    let mut csprng: OsRng = OsRng::new().unwrap();
+    let alice_secret: [u8; 32] = generate_secret(&mut csprng);
+    let bob_secret: [u8; 32] = generate_secret(&mut csprng);
+    let bob_public: [u8; 32] = generate_public(&bob_secret).to_bytes();
+
+    c.bench_function("diffie_hellman", move |b| {
+        b.iter(||
+               diffie_hellman(&alice_secret, &bob_public)
+        )
+    });
+}
+
+criterion_group!{
+    name = x25519_benches;
+    config = Criterion::default();
+    targets =
+        bench_diffie_hellman,
+}
+criterion_main!{
+    x25519_benches,
+}

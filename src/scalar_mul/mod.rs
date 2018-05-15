bab642c5afcf14bff0317f3d09f5f481aabe0f57
Henry de Valence <hdevalence@hdevalence.ca>
2018-05-15 11:22:21 -0700

    Add doc comments

--- src/scalar_mul/mod.rs
+++ src/scalar_mul/mod.rs
@@ -8,6 +8,14 @@
 // - Isis Agora Lovecruft <isis@patternsinthevoid.net>
 // - Henry de Valence <hdevalence@hdevalence.ca>
 
+//! Implementations of various scalar multiplication algorithms.
+//!
+//! Note that all of these implementations use serial code for field
+//! arithmetic with the multi-model strategy described in the
+//! `curve_models` module.  The vectorized AVX2 backend has its own
+//! scalar multiplication implementations, since it only uses one
+//! curve model.
+
 pub mod window;
 
 pub mod variable_base;

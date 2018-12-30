80e72db67765509be7f74464868c6b4b4a11b5c0
Isis Lovecruft <isis@patternsinthevoid.net>
2018-12-30 02:36:49 +0000

    Create new module for constants.

--- src/ed25519.rs
+++ src/ed25519.rs
@@ -1,11 +1,11 @@
 // -*- mode: rust; -*-
 //
 // This file is part of ed25519-dalek.
-// Copyright (c) 2017-2018 Isis Lovecruft
+// Copyright (c) 2017-2018 isis lovecruft
 // See LICENSE for licensing information.
 //
 // Authors:
-// - Isis Agora Lovecruft <isis@patternsinthevoid.net>
+// - isis agora lovecruft <isis@patternsinthevoid.net>
 
 //! A Rust implementation of ed25519 key generation, signing, and verification.
 
@@ -36,29 +36,10 @@ use curve25519_dalek::edwards::CompressedEdwardsY;
 use curve25519_dalek::edwards::EdwardsPoint;
 use curve25519_dalek::scalar::Scalar;
 
+pub use constants::*;
+
 use errors::SignatureError;
 use errors::InternalError;
-
-/// The length of a ed25519 `Signature`, in bytes.
-pub const SIGNATURE_LENGTH: usize = 64;
-
-/// The length of a ed25519 `SecretKey`, in bytes.
-pub const SECRET_KEY_LENGTH: usize = 32;
-
-/// The length of an ed25519 `PublicKey`, in bytes.
-pub const PUBLIC_KEY_LENGTH: usize = 32;
-
-/// The length of an ed25519 `Keypair`, in bytes.
-pub const KEYPAIR_LENGTH: usize = SECRET_KEY_LENGTH + PUBLIC_KEY_LENGTH;
-
-/// The length of the "key" portion of an "expanded" ed25519 secret key, in bytes.
-const EXPANDED_SECRET_KEY_KEY_LENGTH: usize = 32;
-
-/// The length of the "nonce" portion of an "expanded" ed25519 secret key, in bytes.
-const EXPANDED_SECRET_KEY_NONCE_LENGTH: usize = 32;
-
-/// The length of an "expanded" ed25519 key, `ExpandedSecretKey`, in bytes.
-pub const EXPANDED_SECRET_KEY_LENGTH: usize = EXPANDED_SECRET_KEY_KEY_LENGTH + EXPANDED_SECRET_KEY_NONCE_LENGTH;
 
 /// An EdDSA signature.
 ///
--- src/lib.rs
+++ src/lib.rs
@@ -261,6 +261,7 @@ extern crate serde;
 #[cfg(all(test, feature = "serde"))]
 extern crate bincode;
 
+mod constants;
 mod ed25519;
 
 pub mod errors;

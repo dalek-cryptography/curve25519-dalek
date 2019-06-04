cfa09d859fb85efcf6765367fa97cad8d6bd3f1c
Henry de Valence <hdevalence@hdevalence.ca>
2019-06-04 14:29:33 -0700

    Use upstream IFMA intrinsics now that they exist.

--- src/lib.rs
+++ src/lib.rs
@@ -9,17 +9,11 @@
 // - Henry de Valence <hdevalence@hdevalence.ca>
 
 #![no_std]
-#![cfg_attr(
-    any(
-        all(feature = "simd_backend", target_feature = "avx512ifma"),
-        all(feature = "nightly", rustdoc)
-    ),
-    feature(simd_ffi, link_llvm_intrinsics)
-)]
 #![cfg_attr(feature = "nightly", feature(test))]
 #![cfg_attr(all(feature = "alloc", not(feature = "std")), feature(alloc))]
 #![cfg_attr(feature = "nightly", feature(external_doc))]
 #![cfg_attr(feature = "nightly", feature(doc_cfg))]
+#![cfg_attr(feature = "simd_backend", feature(stdsimd))]
 // Refuse to compile if documentation is missing, but only on nightly.
 //
 // This means that missing docs will still fail CI, but means we can use

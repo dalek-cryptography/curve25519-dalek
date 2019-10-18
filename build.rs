bc1ef06e50538be88129ea2d0f4fe61169599331
Da Vinci <@>
2019-10-18 03:10:44 +0300

    Fix Travis Test Feature

--- build.rs
+++ build.rs
@@ -1,5 +1,23 @@
+use rustc_version::{version, version_meta, Channel};
+
 fn main() {
     // XXX this should be replaced with autoselection logic.
     // Enable the "stage2_build" feature in the main build stage
     //println!("cargo:rustc-cfg=feature=\"stage2_build\"\n");
+
+    match version_meta().unwrap().channel {
+        Channel::Stable => {
+            println!("cargo:rustc-cfg=stable");
+        }
+        Channel::Beta => {
+            println!("cargo:rustc-cfg=beta");
+        }
+        Channel::Nightly => {
+            println!("cargo:rustc-cfg=nightly");
+            println!("cargo:rustc-cfg=feature=nightly");
+        }
+        Channel::Dev => {
+            println!("cargo:rustc-cfg=dev");
+        }
+    }
 }

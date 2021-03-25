1491f0db364f8d8fc1072caff88967248c555ef5
Isis Lovecruft <isis@patternsinthevoid.net>
2021-03-25 04:10:55 +0000

    Update copyright years.

--- src/backend/vector/avx2/constants.rs
+++ src/backend/vector/avx2/constants.rs
@@ -1,11 +1,12 @@
 // -*- mode: rust; -*-
 //
 // This file is part of curve25519-dalek.
-// Copyright (c) 2016-2019 Isis Lovecruft, Henry de Valence
+// Copyright (c) 2016-2021 isis lovecruft
+// Copyright (c) 2016-2019 Henry de Valence
 // See LICENSE for licensing information.
 //
 // Authors:
-// - Isis Agora Lovecruft <isis@patternsinthevoid.net>
+// - isis agora lovecruft <isis@patternsinthevoid.net>
 // - Henry de Valence <hdevalence@hdevalence.ca>
 
 //! This module contains constants used by the AVX2 backend.

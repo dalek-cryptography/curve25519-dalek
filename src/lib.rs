d94b0f52dc92bcb597bd01dcd163b85979e2a1ed
gbaranski <root@gbaranski.com>
2021-08-01 18:29:28 +0200

    fix: remove rust-analyzer breaking line

--- src/lib.rs
+++ src/lib.rs
@@ -235,7 +235,6 @@
 #![warn(future_incompatible)]
 #![deny(missing_docs)] // refuse to compile if documentation is missing
 
-#![cfg(not(test))]
 #![forbid(unsafe_code)]
 
 #[cfg(any(feature = "std", test))]

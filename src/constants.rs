b3b733830fdadbbc72016ced5e7f3e0cbfced4a1
Michael Rosenberg <michael@mrosenberg.pub>
2022-10-17 15:48:28 -0400

    Fixed feature-gated `use` statements

--- src/constants.rs
+++ src/constants.rs
@@ -29,19 +29,19 @@
 #![allow(non_snake_case)]
 
 use crate::edwards::CompressedEdwardsY;
-use crate::ristretto::RistrettoPoint;
-use crate::ristretto::CompressedRistretto;
 use crate::montgomery::MontgomeryPoint;
+use crate::ristretto::CompressedRistretto;
+use crate::ristretto::RistrettoPoint;
 use crate::scalar::Scalar;
 
 #[cfg(feature = "fiat_u32_backend")]
-pub use backend::serial::fiat_u32::constants::*;
+pub use crate::backend::serial::fiat_u32::constants::*;
 #[cfg(feature = "fiat_u64_backend")]
-pub use backend::serial::fiat_u64::constants::*;
+pub use crate::backend::serial::fiat_u64::constants::*;
+#[cfg(feature = "u32_backend")]
+pub use crate::backend::serial::u32::constants::*;
 #[cfg(feature = "u64_backend")]
 pub use crate::backend::serial::u64::constants::*;
-#[cfg(feature = "u32_backend")]
-pub use backend::serial::u32::constants::*;
 
 /// The Ed25519 basepoint, in `CompressedEdwardsY` format.
 ///
@@ -49,25 +49,22 @@ pub use backend::serial::u32::constants::*;
 /// which is the \\(y\\)-coordinate of the Ed25519 basepoint.
 ///
 /// The sign bit is 0 since the basepoint has \\(x\\) chosen to be positive.
-pub const ED25519_BASEPOINT_COMPRESSED: CompressedEdwardsY =
-    CompressedEdwardsY([0x58, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66,
-                        0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66,
-                        0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66,
-                        0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66]);
+pub const ED25519_BASEPOINT_COMPRESSED: CompressedEdwardsY = CompressedEdwardsY([
+    0x58, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66,
+    0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66,
+]);
 
 /// The X25519 basepoint, in `MontgomeryPoint` format.
-pub const X25519_BASEPOINT: MontgomeryPoint =
-    MontgomeryPoint([0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+pub const X25519_BASEPOINT: MontgomeryPoint = MontgomeryPoint([
+    0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+]);
 
 /// The Ristretto basepoint, in `CompressedRistretto` format.
-pub const RISTRETTO_BASEPOINT_COMPRESSED: CompressedRistretto =
-    CompressedRistretto([0xe2, 0xf2, 0xae, 0x0a, 0x6a, 0xbc, 0x4e, 0x71,
-                         0xa8, 0x84, 0xa9, 0x61, 0xc5, 0x00, 0x51, 0x5f,
-                         0x58, 0xe3, 0x0b, 0x6a, 0xa5, 0x82, 0xdd, 0x8d,
-                         0xb6, 0xa6, 0x59, 0x45, 0xe0, 0x8d, 0x2d, 0x76]);
+pub const RISTRETTO_BASEPOINT_COMPRESSED: CompressedRistretto = CompressedRistretto([
+    0xe2, 0xf2, 0xae, 0x0a, 0x6a, 0xbc, 0x4e, 0x71, 0xa8, 0x84, 0xa9, 0x61, 0xc5, 0x00, 0x51, 0x5f,
+    0x58, 0xe3, 0x0b, 0x6a, 0xa5, 0x82, 0xdd, 0x8d, 0xb6, 0xa6, 0x59, 0x45, 0xe0, 0x8d, 0x2d, 0x76,
+]);
 
 /// The Ristretto basepoint, as a `RistrettoPoint`.
 ///
@@ -79,25 +76,24 @@ pub const RISTRETTO_BASEPOINT_POINT: RistrettoPoint = RistrettoPoint(ED25519_BAS
 /// $$
 /// \ell = 2^\{252\} + 27742317777372353535851937790883648493.
 /// $$
-pub const BASEPOINT_ORDER: Scalar = Scalar{
+pub const BASEPOINT_ORDER: Scalar = Scalar {
     bytes: [
-        0xed, 0xd3, 0xf5, 0x5c, 0x1a, 0x63, 0x12, 0x58,
-        0xd6, 0x9c, 0xf7, 0xa2, 0xde, 0xf9, 0xde, 0x14,
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10,
+        0xed, 0xd3, 0xf5, 0x5c, 0x1a, 0x63, 0x12, 0x58, 0xd6, 0x9c, 0xf7, 0xa2, 0xde, 0xf9, 0xde,
+        0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x10,
     ],
 };
 
 use crate::ristretto::RistrettoBasepointTable;
 /// The Ristretto basepoint, as a `RistrettoBasepointTable` for scalar multiplication.
-pub const RISTRETTO_BASEPOINT_TABLE: RistrettoBasepointTable
-    = RistrettoBasepointTable(ED25519_BASEPOINT_TABLE);
+pub const RISTRETTO_BASEPOINT_TABLE: RistrettoBasepointTable =
+    RistrettoBasepointTable(ED25519_BASEPOINT_TABLE);
 
 #[cfg(test)]
 mod test {
+    use crate::constants;
     use crate::field::FieldElement;
     use crate::traits::{IsIdentity, ValidityCheck};
-    use crate::constants;
 
     #[test]
     fn test_eight_torsion() {
@@ -131,7 +127,7 @@ mod test {
     fn test_sqrt_minus_one() {
         let minus_one = FieldElement::minus_one();
         let sqrt_m1_sq = &constants::SQRT_M1 * &constants::SQRT_M1;
-        assert_eq!(minus_one,  sqrt_m1_sq);
+        assert_eq!(minus_one, sqrt_m1_sq);
         assert_eq!(constants::SQRT_M1.is_negative().unwrap_u8(), 0);
     }
 
@@ -140,7 +136,7 @@ mod test {
         let minus_one = FieldElement::minus_one();
         let (was_nonzero_square, invsqrt_m1) = minus_one.invsqrt();
         assert_eq!(was_nonzero_square.unwrap_u8(), 1u8);
-        let sign_test_sqrt  = &invsqrt_m1 * &constants::SQRT_M1;
+        let sign_test_sqrt = &invsqrt_m1 * &constants::SQRT_M1;
         assert_eq!(sign_test_sqrt, minus_one);
     }
 
@@ -149,8 +145,8 @@ mod test {
     #[cfg(feature = "u32_backend")]
     fn test_d_vs_ratio() {
         use backend::serial::u32::field::FieldElement2625;
-        let a = -&FieldElement2625([121665,0,0,0,0,0,0,0,0,0]);
-        let b =   FieldElement2625([121666,0,0,0,0,0,0,0,0,0]);
+        let a = -&FieldElement2625([121665, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        let b = FieldElement2625([121666, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
         let d = &a * &b.invert();
         let d2 = &d + &d;
         assert_eq!(d, constants::EDWARDS_D);
@@ -162,8 +158,8 @@ mod test {
     #[cfg(feature = "u64_backend")]
     fn test_d_vs_ratio() {
         use crate::backend::serial::u64::field::FieldElement51;
-        let a = -&FieldElement51([121665,0,0,0,0]);
-        let b =   FieldElement51([121666,0,0,0,0]);
+        let a = -&FieldElement51([121665, 0, 0, 0, 0]);
+        let b = FieldElement51([121666, 0, 0, 0, 0]);
         let d = &a * &b.invert();
         let d2 = &d + &d;
         assert_eq!(d, constants::EDWARDS_D);
@@ -177,5 +173,4 @@ mod test {
         let should_be_ad_minus_one = constants::SQRT_AD_MINUS_ONE.square();
         assert_eq!(should_be_ad_minus_one, ad_minus_one);
     }
-
 }

ed5629a840670085e35d1619a7d29cfc8f47bd20
Michael Rosenberg <michael@mrosenberg.pub>
2022-11-21 17:14:57 -0500

    Redid docs and naming of the old EdwardPoint::hash_to_curve

--- src/edwards.rs
+++ src/edwards.rs
@@ -534,14 +534,16 @@ impl EdwardsPoint {
         CompressedEdwardsY(s)
     }
 
-    /// Hashes the input and maps the digest to the curve. This is NOT a hash-to-curve function, as
-    /// it produces points with a non-uniform distribution.
-    // See https://tools.ietf.org/html/draft-irtf-cfrg-hash-to-curve-10#section-6.7.1
+    /// Maps the digest of the input bytes to the curve. This is NOT a hash-to-curve function, as
+    /// it produces points with a non-uniform distribution. Rather, it performs something that
+    /// resembles (but is not) half of the
+    /// [`hash_to_curve`](https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-16.html#section-3-4.2.1)
+    /// function from the Elligator2 spec.
     #[deprecated(
         since = "4.0.0",
         note = "previously named `hash_from_bytes`, this is not a secure hash function"
     )]
-    pub fn hash_and_encode<D>(bytes: &[u8]) -> EdwardsPoint
+    pub fn nonspec_map_to_curve<D>(bytes: &[u8]) -> EdwardsPoint
     where
         D: Digest<OutputSize = U64> + Default,
     {
@@ -1730,7 +1732,7 @@ mod test {
             let input = hex::decode(vector[0]).unwrap();
             let output = hex::decode(vector[1]).unwrap();
 
-            let point = EdwardsPoint::hash_and_encode::<sha2::Sha512>(&input);
+            let point = EdwardsPoint::nonspec_map_to_curve::<sha2::Sha512>(&input);
             assert_eq!(point.compress().to_bytes(), output[..]);
         }
     }

7113f5f57adc4aa784eff2640d1c65628d1b7d4b
Michael Rosenberg <michael@mrosenberg.pub>
2022-12-08 16:22:46 -0500

    Made Scalar::bits_le iterate over bool, not i8

--- src/scalar.rs
+++ src/scalar.rs
@@ -842,12 +842,12 @@ impl Scalar {
     }
 
     /// Get the bits of the scalar, in little-endian order
-    pub(crate) fn bits_le(&self) -> impl DoubleEndedIterator<Item = i8> + '_ {
+    pub(crate) fn bits_le(&self) -> impl DoubleEndedIterator<Item = bool> + '_ {
         (0..256).map(|i| {
             // As i runs from 0..256, the bottom 3 bits index the bit, while the upper bits index
             // the byte. Since self.bytes is little-endian at the byte level, this iterator is
             // little-endian on the bit level
-            ((self.bytes[i >> 3] >> (i & 7)) & 1u8) as i8
+            ((self.bytes[i >> 3] >> (i & 7)) & 1u8) == 1
         })
     }
 

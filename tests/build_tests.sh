07d07559fb7eee8a2d06355869b387637399ef4c
pinkforest <36498018+pinkforest@users.noreply.github.com>
2023-06-13 15:33:55 +0000

    Ensure no incremental compilation for build script tests

--- tests/build_tests.sh
+++ tests/build_tests.sh
@@ -16,55 +16,64 @@ function match_and_report() {
 }
 
 # Assuming naively 64 bit host
-OUT=build_default.txt
+cargo clean
+OUT=build_1.txt
 env RUSTFLAGS="--cfg curve25519_dalek_diagnostics=\"build\"" cargo build > "$OUT" 2>&1
 match_and_report "curve25519_dalek_backend is 'auto'" "$OUT"
 match_and_report "curve25519_dalek_bits is '64'" "$OUT"
 
 # Override to 32 bits assuming naively 64 bit build host
-OUT=build_default_32.txt
+cargo clean
+OUT=build_2.txt
 env RUSTFLAGS="--cfg curve25519_dalek_diagnostics=\"build\" --cfg curve25519_dalek_bits=\"32\"" cargo build > "$OUT" 2>&1
 match_and_report "curve25519_dalek_backend is 'auto'" "$OUT"
 match_and_report "curve25519_dalek_bits is '32'" "$OUT"
 
 # Override to 64 bits on 32 bit target
-OUT=build_default_64_on_32.txt
+cargo clean
+OUT=build_3.txt
 env RUSTFLAGS="--cfg curve25519_dalek_diagnostics=\"build\" --cfg curve25519_dalek_bits=\"64\"" cargo build --target i686-unknown-linux-gnu > "$OUT" 2>&1
 match_and_report "curve25519_dalek_backend is 'auto'" "$OUT"
 match_and_report "curve25519_dalek_bits is '64'" "$OUT"
 
 # 32 bit target default
-OUT=build_default_32_on_32.txt
+cargo clean
+OUT=build_4.txt
 env RUSTFLAGS="--cfg curve25519_dalek_diagnostics=\"build\"" cargo build --target i686-unknown-linux-gnu > "$OUT" 2>&1
 match_and_report "curve25519_dalek_backend is 'auto'" "$OUT"
 match_and_report "curve25519_dalek_bits is '32'" "$OUT"
 
 # wasm 32 bit target default
-OUT=build_default_wasm_32.txt
+cargo clean
+OUT=build_5.txt
 env RUSTFLAGS="--cfg curve25519_dalek_diagnostics=\"build\"" cargo build --target wasm32-unknown-unknown > "$OUT" 2>&1
 match_and_report "curve25519_dalek_backend is 'auto'" "$OUT"
 match_and_report "curve25519_dalek_bits is '32'" "$OUT"
 
 # fiat override with default 64 bit naive host assumption
-OUT=build_fiat_64.txt
+cargo clean
+OUT=build_6.txt
 env RUSTFLAGS="--cfg curve25519_dalek_diagnostics=\"build\" --cfg curve25519_dalek_backend=\"fiat\"" cargo build > "$OUT" 2>&1
 match_and_report "curve25519_dalek_backend is 'fiat'" "$OUT"
 match_and_report "curve25519_dalek_bits is '64'" "$OUT"
 
 # fiat 32 bit override
-OUT=build_fiat_32.txt
+cargo clean
+OUT=build_7.txt
 env RUSTFLAGS="--cfg curve25519_dalek_diagnostics=\"build\" --cfg curve25519_dalek_backend=\"fiat\" --cfg curve25519_dalek_bits=\"32\"" cargo build > "$OUT" 2>&1
 match_and_report "curve25519_dalek_backend is 'fiat'" "$OUT"
 match_and_report "curve25519_dalek_bits is '32'" "$OUT"
 
 # serial override with default 64 bit naive host assumption
-OUT=build_serial_64.txt
+cargo clean
+OUT=build_8.txt
 env RUSTFLAGS="--cfg curve25519_dalek_diagnostics=\"build\" --cfg curve25519_dalek_backend=\"serial\"" cargo build > "$OUT" 2>&1
 match_and_report "curve25519_dalek_backend is 'serial'" "$OUT"
 match_and_report "curve25519_dalek_bits is '64'" "$OUT"
 
 # serial 32 bit override
-OUT=build_serial_32.txt
+cargo clean
+OUT=build_9.txt
 env RUSTFLAGS="--cfg curve25519_dalek_diagnostics=\"build\" --cfg curve25519_dalek_backend=\"serial\" --cfg curve25519_dalek_bits=\"32\"" cargo build > "$OUT" 2>&1
 match_and_report "curve25519_dalek_backend is 'serial'" "$OUT"
 match_and_report "curve25519_dalek_bits is '32'" "$OUT"

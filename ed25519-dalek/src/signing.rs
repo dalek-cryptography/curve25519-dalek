7cbf86c7d8d8b34c60c057808f3b7f04ebce7036
Rob Ede <robjtede@icloud.com>
2023-07-02 18:42:02 +0100

    Add StreamVerifier

--- ed25519-dalek/src/signing.rs
+++ ed25519-dalek/src/signing.rs
@@ -44,7 +44,7 @@ use crate::{
     errors::{InternalError, SignatureError},
     hazmat::ExpandedSecretKey,
     signature::InternalSignature,
-    verifying::VerifyingKey,
+    verifying::{StreamVerifier, VerifyingKey},
     Signature,
 };
 
@@ -481,6 +481,16 @@ impl SigningKey {
         signature: &Signature,
     ) -> Result<(), SignatureError> {
         self.verifying_key.verify_strict(message, signature)
+    }
+
+    /// Constructs stream verifier with candidate `signature`.
+    ///
+    /// See [`VerifyingKey::verify_stream()`] for more details.
+    pub fn verify_stream(
+        &self,
+        signature: &ed25519::Signature,
+    ) -> Result<StreamVerifier, SignatureError> {
+        self.verifying_key.verify_stream(signature)
     }
 
     /// Convert this signing key into a byte representation of an unreduced, unclamped Curve25519

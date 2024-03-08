b1ebd5c6e06e606eee0023f4c240892e838af263
pinkforest <36498018+pinkforest@users.noreply.github.com>
2024-03-08 12:22:14 +1100

    Clean up couple of cfg(test) gates

--- wycheproofs/wycheproof-ed25519-dalek/src/lib.rs
+++ wycheproofs/wycheproof-ed25519-dalek/src/lib.rs
@@ -1,6 +1,8 @@
+#[cfg(test)]
 use ed25519_dalek::{VerifyingKey, Verifier, Signature};
 
 // hex strings (due to generated source files being ascii
+#[cfg(test)]
 fn ed25519_dalek_wycheproof(public_key: &str, sig: &str, msg: &str) {
 
     let pk_bytes = hex::decode(public_key).unwrap();
@@ -12,7 +14,7 @@ fn ed25519_dalek_wycheproof(public_key: &str, sig: &str, msg: &str) {
     // panic if size wrong
     let sig: &[u8; 64] = sig_bytes.as_slice().try_into().expect("input sig incorrect length");
 
-    let typed_sig = ed25519_dalek::Signature::from_bytes(sig);
+    let typed_sig = Signature::from_bytes(sig);
     let verifying_key = VerifyingKey::from_bytes(pk).unwrap();
     
     assert!(verifying_key.verify(hex::decode(msg).unwrap().as_slice(), &typed_sig).is_ok())

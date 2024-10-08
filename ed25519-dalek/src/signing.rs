43a16f03d4c635a8836c23ac07244c116ea3aab8
Julius Liu <juliusl@microsoft.com>
2024-10-07 17:01:13 -0700

    Implement `DynSignatureAlgorithmIdentifier` trait for ed25519 (#712)

--- ed25519-dalek/src/signing.rs
+++ ed25519-dalek/src/signing.rs
@@ -665,6 +665,20 @@ impl pkcs8::EncodePrivateKey for SigningKey {
     }
 }
 
+#[cfg(all(feature = "alloc", feature = "pkcs8"))]
+impl pkcs8::spki::DynSignatureAlgorithmIdentifier for SigningKey {
+    fn signature_algorithm_identifier(
+        &self,
+    ) -> pkcs8::spki::Result<pkcs8::spki::AlgorithmIdentifierOwned> {
+        // From https://datatracker.ietf.org/doc/html/rfc8410
+        // `id-Ed25519   OBJECT IDENTIFIER ::= { 1 3 101 112 }`
+        Ok(pkcs8::spki::AlgorithmIdentifier {
+            oid: ed25519::pkcs8::ALGORITHM_OID,
+            parameters: None,
+        })
+    }
+}
+
 #[cfg(feature = "pkcs8")]
 impl TryFrom<pkcs8::KeypairBytes> for SigningKey {
     type Error = pkcs8::Error;

8709d772106513eedc55e9ffb546e6f912541579
iquerejeta <querejeta.azurmendi@iohk.io>
2025-05-26 11:27:38 +0200

    Nits

--- curve25519-dalek/src/edwards.rs
+++ curve25519-dalek/src/edwards.rs
@@ -104,11 +104,10 @@ use core::ops::{Add, Neg, Sub};
 use core::ops::{AddAssign, SubAssign};
 use core::ops::{Mul, MulAssign};
 
-use digest::consts::True;
-use digest::typenum::IsGreater;
 #[cfg(feature = "digest")]
 use digest::{
-    crypto_common::BlockSizeUser, generic_array::typenum::U64, Digest, FixedOutput, HashMarker,
+    consts::True, crypto_common::BlockSizeUser, generic_array::typenum::U64, typenum::IsGreater,
+    Digest, FixedOutput, HashMarker,
 };
 
 #[cfg(feature = "group")]

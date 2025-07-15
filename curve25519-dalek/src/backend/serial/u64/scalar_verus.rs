f15b560ffb441eb7bcc816d36c092fcfee6885a2
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-07-15 16:51:15 +0100

    Shorten proof

--- curve25519-dalek/src/backend/serial/u64/scalar_verus.rs
+++ curve25519-dalek/src/backend/serial/u64/scalar_verus.rs
@@ -1,6 +1,7 @@
 // scalar64_verus.rs
 #![allow(unused)]
 use vstd::prelude::*;
+use vstd::arithmetic::mul::lemma_mul_by_zero_is_zero;
 use vstd::arithmetic::mul::lemma_mul_strict_inequality;
 use vstd::calc;
 use vstd::arithmetic::power2::*;
@@ -173,15 +174,10 @@ verus! {
             z == x * y
         {
             proof {
-                assert(x < (1u64 << 52));
-                assert(y < (1u64 << 52));
-                assert((x as u128) == x as u64);
-                assert((y as u128) == y as u64);
                 assert(1u128 << 52 == 1u64 << 52) by (bit_vector);
                 assert((x as u128) < (1u128 << 52));
                 assert((x as u128) * (1u128 << 52) < (1u128 << 52) * (1u128 << 52)) by
                   {lemma_mul_strict_inequality(x as int, (1u128 << 52) as int, (1u128 << 52) as int)};
-                assert((y as u128) < (1u128 << 52));
                 assert((x as u128) * (y as u128) <= (x as u128) * (1u128 << 52)) by
                   {
 
@@ -190,8 +186,8 @@ verus! {
                       lemma_mul_strict_inequality(y as int, (1u128 << 52) as int, x as int)
                     } else {
                         assert (x == 0);
-                        assert ((x as u128) * (y as u128) == 0);
-                        assert ((x as u128) * (1u128 << 52) == 0);
+                        assert ((x as u128) * (y as u128) == 0) by {lemma_mul_by_zero_is_zero(x as int)};
+                        assert ((x as u128) * (1u128 << 52) == 0) by {lemma_mul_by_zero_is_zero(x as int)};
                     }
                   };
                 assert((x as u128) * (y as u128) < (1u128 << 52) * (1u128 << 52));

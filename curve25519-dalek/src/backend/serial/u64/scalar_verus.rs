7a0600c89aa5777fb4b2a28ec8821c11eaf7c09d
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-07-15 16:53:23 +0100

    Tweak proof

--- curve25519-dalek/src/backend/serial/u64/scalar_verus.rs
+++ curve25519-dalek/src/backend/serial/u64/scalar_verus.rs
@@ -175,12 +175,11 @@ verus! {
             proof {
                 assert(1u128 << 52 == 1u64 << 52) by (bit_vector);
                 assert((x as u128) < (1u128 << 52));
+                assert((y as u128) < (1u128 << 52));
                 assert((x as u128) * (1u128 << 52) < (1u128 << 52) * (1u128 << 52)) by
                   {lemma_mul_strict_inequality(x as int, (1u128 << 52) as int, (1u128 << 52) as int)};
-                assert((y as u128) < (1u128 << 52));
                 assert((x as u128) * (y as u128) <= (x as u128) * (1u128 << 52)) by
                   {
-
                     if x > 0
                     {
                       lemma_mul_strict_inequality(y as int, (1u128 << 52) as int, x as int)
@@ -191,8 +190,7 @@ verus! {
                     }
                   };
                 assert((x as u128) * (y as u128) < (1u128 << 52) * (1u128 << 52));
-                assert((1u128 << 52) * (1u128 << 52) <= (1u128 << 104)) by (compute);
-                assert((x as u128) * (y as u128) < (1u128 << 104));
+                assert((1u128 << 52) * (1u128 << 52) == (1u128 << 104)) by (compute);
             }
             (x as u128) * (y as u128)
         }

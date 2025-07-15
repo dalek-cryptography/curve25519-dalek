82b634c16eed5635621aa0037f7b1c535328868c
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-07-15 16:54:23 +0100

    Reorder proof

--- curve25519-dalek/src/backend/serial/u64/scalar_verus.rs
+++ curve25519-dalek/src/backend/serial/u64/scalar_verus.rs
@@ -176,8 +176,6 @@ verus! {
                 assert(1u128 << 52 == 1u64 << 52) by (bit_vector);
                 assert((x as u128) < (1u128 << 52));
                 assert((y as u128) < (1u128 << 52));
-                assert((x as u128) * (1u128 << 52) < (1u128 << 52) * (1u128 << 52)) by
-                  {lemma_mul_strict_inequality(x as int, (1u128 << 52) as int, (1u128 << 52) as int)};
                 assert((x as u128) * (y as u128) <= (x as u128) * (1u128 << 52)) by
                   {
                     if x > 0
@@ -189,6 +187,8 @@ verus! {
                         assert ((x as u128) * (1u128 << 52) == 0);
                     }
                   };
+                assert((x as u128) * (1u128 << 52) < (1u128 << 52) * (1u128 << 52)) by
+                  {lemma_mul_strict_inequality(x as int, (1u128 << 52) as int, (1u128 << 52) as int)};
                 assert((1u128 << 52) * (1u128 << 52) == (1u128 << 104)) by (compute);
             }
             (x as u128) * (y as u128)

bfb1e1fb22083655520bdd0102857bdbd400f7de
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-07-30 10:31:12 +0100

    Add todo

--- curve25519-dalek/src/backend/serial/u64/scalar.rs
+++ curve25519-dalek/src/backend/serial/u64/scalar.rs
@@ -255,6 +255,7 @@ impl Zeroize for Scalar52 {
 verus!{
 impl Index<usize> for Scalar52 {
     type Output = u64;
+    // TODO Do we need this?
     #[verifier::external_body]
     fn index(&self, _index: usize) -> &u64 {
         &(self.limbs[_index])

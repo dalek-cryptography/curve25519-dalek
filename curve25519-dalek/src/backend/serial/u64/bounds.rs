c93d5827205ae87fe0e95a2029a447d0e43a92fa
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-07-31 11:44:48 +0100

    Prune the lemma

--- curve25519-dalek/src/backend/serial/u64/bounds.rs
+++ curve25519-dalek/src/backend/serial/u64/bounds.rs
@@ -17,12 +17,9 @@ ensures (x as u128) * (y as u128) < (1u128 << 104)
         (x as u128) * (y as u128); (<=) {
             if x > 0 {
                 lemma_mul_strict_inequality(y as int, (1u128 << 52) as int, x as int);
-                assert( y * x < (1u128 << 52) * x );
                 assert( x * y < x * (1u128 << 52)  );
             } else {
-                assert(x == 0);
                 assert((0 as u128) * (y as u128) == 0);
-                assert((x as u128) * (y as u128) == 0);
             }
         }
         (x as u128) * (1u128 << 52); (<) {

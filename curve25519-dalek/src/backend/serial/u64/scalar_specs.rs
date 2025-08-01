7752f1f8dedb0f068f5ad1ea5e599bf7da6b1698
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-08-01 12:45:19 +0100

    Need to fix CI anyway

--- curve25519-dalek/src/backend/serial/u64/scalar_specs.rs
+++ curve25519-dalek/src/backend/serial/u64/scalar_specs.rs
@@ -1,5 +1,5 @@
 #[allow(unused_imports)]
-use crate::backend::serial::u64::scalar::Scalar52;
+use super::scalar::Scalar52;
 #[allow(unused_imports)]
 use vstd::arithmetic::power2::*;
 use vstd::prelude::*;

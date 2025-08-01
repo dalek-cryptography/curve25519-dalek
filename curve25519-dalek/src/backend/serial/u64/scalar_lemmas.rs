72c72dafa0c311b1673ad8d9e52c72b9abe2ca64
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-08-01 16:20:15 +0100

    Add comment

--- curve25519-dalek/src/backend/serial/u64/scalar_lemmas.rs
+++ curve25519-dalek/src/backend/serial/u64/scalar_lemmas.rs
@@ -377,6 +377,7 @@ pub proof fn lemma_rr_limbs_bounded()
     ensures
         0x000d63c715bea69fu64 < (1u64 << 52),
 {
+    // Verus can figure that out the other 4 limbs are bounded
     assert(0x000d63c715bea69fu64 < (1u64 << 52)) by (bit_vector);
 }
 

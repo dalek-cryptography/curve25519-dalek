4ef07251d84a90a96a2203f7e969aeb79a79368e
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-08-10 12:05:57 +0100

    Add another assert to satisfy precondition

--- curve25519-dalek/src/backend/serial/u64/scalar.rs
+++ curve25519-dalek/src/backend/serial/u64/scalar.rs
@@ -336,6 +336,7 @@ impl Scalar52 {
             //                         seq_u64_to_nat(difference.limbs@.subrange(0, i as int )) - (old_borrow >> 63) * pow2((52 * (i) as nat))
             // );
             // CLAUDE
+            assert(difference_loop1_start.limbs@.subrange(0, i as int) == difference.limbs@.subrange(0, i as int));
             assert(
             seq_u64_to_nat(a.limbs@.subrange(0, i as int)) - seq_u64_to_nat(b.limbs@.subrange(0, i as int )) ==
                         seq_u64_to_nat(difference_loop1_start.limbs@.subrange(0, i as int )) - (old_borrow >> 63) * pow2((52 * (i) as nat)));
